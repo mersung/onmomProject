285e4f8344dda577e6150355fa639f6f57cf08db
ShimDaeSung1 <tlaeotjd12@ajou.ac.kr>
2022-06-17 15:29:52 +0900

    처음 세팅 완료, gradle, application

---
+++ src/main/java/org/zerock/onmomProject/OnmomProjectApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.onmomProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnmomProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnmomProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/zerock/onmomProject/OnmomProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.onmomProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnmomProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
