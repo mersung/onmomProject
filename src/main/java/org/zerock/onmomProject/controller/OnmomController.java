a016a3d7a3f86c4392886416dad7e91f18cc7618
riiine <xpzmslr98@naver.com>
2022-06-20 11:07:19 +0900

    resources 폴더에 부트스트랩 템플릿 추가함 http://localhost:8181/onmom/index 실행되는 것 확인했음

---
+++ src/main/java/org/zerock/onmomProject/controller/OnmomController.java
@@ -0,0 +1,17 @@
+package org.zerock.onmomProject.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/onmom")
+@Log4j2
+public class OnmomController {
+
+    @GetMapping("/index")
+    public void index(){
+        log.info("index.............");
+    }
+}
