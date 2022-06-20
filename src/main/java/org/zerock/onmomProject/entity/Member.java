7d22b62f4efbbda1dfde511fc58db431a2c9afce
seokjuna <lukas95@naver.com>
2022-06-20 16:45:53 +0900

    seokju

---
+++ src/main/java/org/zerock/onmomProject/entity/Member.java
@@ -0,0 +1,26 @@
+package org.zerock.onmomProject.entity;
+
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class Member extends BaseEntity {
+
+    @Id
+    private String member_id;
+
+    private String nickname;
+
+    private String pw;
+
+    private MemberRole role;
+
+}
