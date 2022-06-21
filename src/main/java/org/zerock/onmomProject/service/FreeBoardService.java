310582fdef93d11fa3c8d47ef3b9c2b783df61a4
windowgod <91017486+windowgod@users.noreply.github.com>
2022-06-21 16:51:04 +0900

    22/06/21 1-4 FreeBoardService 추가 작성 중 FreeBoardServiceImpl 추가 작성 중

--- src/main/java/org/zerock/onmomProject/service/FreeBoardService.java
+++ src/main/java/org/zerock/onmomProject/service/FreeBoardService.java
@@ -8,14 +8,30 @@ public interface FreeBoardService {
     Long register(FreeBoardDTO dto);
 
     default FreeBoard dtoToEntity(FreeBoardDTO dto){
-        Member member = Member.builder().member_id(dto.getWriterMember_id()).build();
+        Member member = Member.builder().member_id(dto.getMember_id()).build();
 
-        FreeBoard board = FreeBoard.builder()
+        FreeBoard freeBoard = FreeBoard.builder()
                 .free_id(dto.getFree_id())
                 .title(dto.getTitle())
                 .content(dto.getContent())
                 .member_id(member)
                 .build();
-        return board;
+        return freeBoard;
+    }
+    default FreeBoardDTO entityToDTO(FreeBoard freeBoard, Member member, Long replyCount) {
+
+        FreeBoardDTO freeBoardDTO = FreeBoardDTO.builder()
+                .free_id(freeBoard.getFree_id())
+                .title(freeBoard.getTitle())
+                .content(freeBoard.getContent())
+                .regDate(freeBoard.getRegDate())
+                .modDate(freeBoard.getModDate())
+                .member_id(member.getMember_id())
+                .like_cnt(freeBoard.getLike_cnt()) //int로 처리하도록
+                .hate_cnt(freeBoard.getHate_cnt())
+                .build();
+
+        return freeBoardDTO;
+
     }
 }
