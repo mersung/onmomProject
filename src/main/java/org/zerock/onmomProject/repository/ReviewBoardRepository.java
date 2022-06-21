6b8dd1b33211b00aeb977185a9102e7c7edaf3f4
ShimDaeSung1 <tlaeotjd12@ajou.ac.kr>
2022-06-21 13:32:59 +0900

    검색 Repository만들기

--- src/main/java/org/zerock/onmomProject/repository/ReviewBoardRepository.java
+++ src/main/java/org/zerock/onmomProject/repository/ReviewBoardRepository.java
@@ -2,6 +2,8 @@ package org.zerock.onmomProject.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.zerock.onmomProject.entity.ReviewBoard;
+import org.zerock.onmomProject.repository.search.SearchReviewBoardRepository;
 
-public interface ReviewBoardRepository extends JpaRepository<ReviewBoard, Long> {
+public interface ReviewBoardRepository extends JpaRepository<ReviewBoard, Long>, SearchReviewBoardRepository {
+
 }
