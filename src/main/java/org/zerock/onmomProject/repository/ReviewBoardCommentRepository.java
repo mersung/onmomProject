c6a55cb8bf5b60ad13f8e38bb5996f7cbf041c02
windowgod <91017486+windowgod@users.noreply.github.com>
2022-06-21 15:46:09 +0900

    22/06/21 1-1 FreeRepository 작성 FreeBoardService 작성 FreeBoardServiceImpl 작성

--- src/main/java/org/zerock/onmomProject/repository/ReviewBoardCommentRepository.java
+++
@@ -1,7 +0,0 @@
-package org.zerock.onmomProject.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.zerock.onmomProject.entity.ReviewBoardComment;
-
-public interface ReviewBoardCommentRepository extends JpaRepository<ReviewBoardComment, Long> {
-}
