e86b87db98ffb62edc5f8f650b20f8c5e2711133
ShimDaeSung1 <tlaeotjd12@ajou.ac.kr>
2022-06-21 09:49:46 +0900

    entity 생성

---
+++ src/main/java/org/zerock/onmomProject/repository/FreeBoardCommentRepository.java
@@ -0,0 +1,7 @@
+package org.zerock.onmomProject.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.zerock.onmomProject.entity.FreeBoardComment;
+
+public interface FreeBoardCommentRepository extends JpaRepository<FreeBoardComment, Long> {
+}
---
+++ src/main/java/org/zerock/onmomProject/repository/FreeBoardRepository.java
@@ -0,0 +1,8 @@
+package org.zerock.onmomProject.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.zerock.onmomProject.entity.FreeBoard;
+
+public interface FreeBoardRepository extends JpaRepository<FreeBoard, Long> {
+    
+}
