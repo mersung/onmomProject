7d23fa668723537987f7fa5e7177bbf52651b72c
mhhjo <gurwhals@naver.com>
2022-06-21 17:24:03 +0900

    6/21 오후 5:23

---
+++ src/main/java/org/zerock/onmomProject/controller/ReviewCommentController.java
@@ -0,0 +1,13 @@
+package org.zerock.onmomProject.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reviews")
+@Log4j2
+@RequiredArgsConstructor
+public class ReviewCommentController {
+}
--- src/main/java/org/zerock/onmomProject/repository/FreeBoardRepository.java
+++ src/main/java/org/zerock/onmomProject/repository/FreeBoardRepository.java
@@ -17,11 +17,11 @@ public interface FreeBoardRepository extends JpaRepository<FreeBoard, Long> {
     @Query("SELECT fb, fbc FROM FreeBoard fb LEFT JOIN FreeBoardComment fbc ON fbc.board_id = fb WHERE fb.free_id = :free_id")
     List<Object[]> getFreeBoardWithFreeBoardComment(@Param("free_id") Long free_id);
 
-    @Query(value = "select fb,m_id count(fbc)" +
-                    "from FreeBoard fb" +
-                    "left join fb.member_id m_id" +
-                    "left join FreeBoardComment fbc ON fbc.board_id = fb" +
-                    "group by fb",
+    @Query(value = "select fb,m_id count(fbc) " +
+                    " from FreeBoard fb" +
+                    " left join fb.member_id m_id" +
+                    " left join FreeBoardComment fbc ON fbc.board_id = fb" +
+                    " group by fb",
                     countQuery = "select count(fb) from FreeBoard fb")
     Page<Object[]> getFreeBoardWithFreeBoardCommentCount(Pageable pageable);
 }
