17098e0fe034d482f401dc3f2594813071e51a09
ShimDaeSung1 <tlaeotjd12@ajou.ac.kr>
2022-06-21 13:32:59 +0900

    검색 Repository만들기

---
+++ src/main/java/org/zerock/onmomProject/repository/search/SearchReviewBoardRepository.java
@@ -0,0 +1,9 @@
+package org.zerock.onmomProject.repository.search;
+
+import org.zerock.onmomProject.entity.ReviewBoard;
+
+public interface SearchReviewBoardRepository {
+    ReviewBoard search1();
+
+
+}
