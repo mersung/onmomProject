847f63ebd47cfe1cf9896f5362adfefd1f4b32d9
ShimDaeSung1 <tlaeotjd12@ajou.ac.kr>
2022-06-21 16:37:49 +0900

    리뷰게시판 검색 Repository 거의 완료

--- src/main/java/org/zerock/onmomProject/OnmomProjectApplication.java
+++ src/main/java/org/zerock/onmomProject/OnmomProjectApplication.java
@@ -3,8 +3,10 @@ package org.zerock.onmomProject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class OnmomProjectApplication {
 
 	public static void main(String[] args) {
