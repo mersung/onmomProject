62b6ea9c8839610b231714960179c86adfa5ea7f
seokjuna <lukas95@naver.com>
2022-06-23 15:14:53 +0900

    로그인 게시판 시작

---
+++ src/main/java/org/zerock/onmomProject/config/SecurityConfig.java
@@ -0,0 +1,55 @@
+package org.zerock.onmomProject.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@Log4j2
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+//    @Autowired
+//    private OnmomUserDetailsService userDetailsService;
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+//        http.authorizeRequests()
+//                .antMatchers("/sample/all").permitAll()
+//                .antMatchers("/sample/member").hasRole("USER");
+//        http.formLogin();
+
+        http.formLogin().loginPage("/sample/login").loginProcessingUrl("/login");
+//        http.csrf().disable();
+
+//        http.oauth2Login().successHandler(successHandler());
+//        http.rememberMe().tokenValiditySeconds(60 * 60 * 7).userDetailsService(userDetailsService);  //7days
+//        http.logout()
+//                .logoutSuccessUrl("/login");
+//    }
+
+//    @Bean
+//    public ClubLoginSuccessHandler successHandler() {
+//        return new ClubLoginSuccessHandler(passwordEncoder());
+//    }
+
+
+    }
+}
+
