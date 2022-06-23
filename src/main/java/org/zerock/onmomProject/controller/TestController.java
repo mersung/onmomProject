fc53bb747f2cd7126bf19d09e5493d095836c5b9
riiine <xpzmslr98@naver.com>
2022-06-23 09:40:18 +0900

    1) TestController.java 작성 2) 후기게시판 html 파일 수정

---
+++ src/main/java/org/zerock/onmomProject/controller/TestController.java
@@ -0,0 +1,28 @@
+package org.zerock.onmomProject.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/onmom")
+@Log4j2
+public class TestController {
+
+    // 후기게시판 등록 페이지 테스트
+    @GetMapping("/review_register")
+    public void review_register(){
+    }
+
+    // 후기게시판 수정 페이지 테스트
+    @GetMapping("/review_modify")
+    public void review_modify(){
+    }
+
+    // 후기게시판 읽기 페이지 테스트
+    @GetMapping("/review_read")
+    public void review_read(){
+    }
+
+}
