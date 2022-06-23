23e71c8424eba9570fa1c3c5c0e1c8deaafb96fc
seokjuna <lukas95@naver.com>
2022-06-23 17:37:25 +0900

    AuthMemberDTO

--- src/main/java/org/zerock/onmomProject/repository/MemberRepository.java
+++ src/main/java/org/zerock/onmomProject/repository/MemberRepository.java
@@ -3,11 +3,14 @@ package org.zerock.onmomProject.repository;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.zerock.onmomProject.entity.Member;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, String> {
-
+    @EntityGraph(attributePaths = {"roleSet"}, type = EntityGraph.EntityGraphType.LOAD)
+    @Query("select m from Member m where m.member_id =:member_id")
+    Optional<Member> findByEmail(@Param("email") String email);
 
 }
