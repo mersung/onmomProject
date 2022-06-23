1cdaeeb0965ed8a67f22d5c41fd0ae6c0b5b3493
ShimDaeSung1 <tlaeotjd12@ajou.ac.kr>
2022-06-23 17:53:43 +0900

    프리보드커멘트레포지토리 수정

---
+++ src/test/java/org/zerock/onmomProject/repository/ReviewCommentRepositoryTests.java
@@ -0,0 +1,20 @@
+//package org.zerock.onmomProject.repository;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.zerock.onmomProject.entity.ReviewBoardComment;
+//
+//import java.util.stream.IntStream;
+//
+//@SpringBootTest
+//public class ReviewCommentRepositoryTests {
+//
+//    @Autowired
+//    private ReviewCommentRepository repository;
+//
+//    public void insert(){
+//        IntStream.rangeClosed()
+//
+//    }
+//
+//}
