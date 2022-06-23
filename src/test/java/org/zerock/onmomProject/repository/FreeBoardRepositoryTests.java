0a27149f4adbfd3319a72a96ec8df70c1c9d9c53
windowgod <91017486+windowgod@users.noreply.github.com>
2022-06-23 17:24:03 +0900

    22-06-23  ======================================= ※수정 사항 ====================================== FreeBoardRepositoryTests 작성중
    FreeBoardCommentRepositoryTests 작성중

---
+++ src/test/java/org/zerock/onmomProject/repository/FreeBoardRepositoryTests.java
@@ -0,0 +1,40 @@
+package org.zerock.onmomProject.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.onmomProject.entity.FreeBoard;
+import org.zerock.onmomProject.entity.Member;
+
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class FreeBoardRepositoryTests {
+
+    @Autowired
+    private FreeBoardRepository freeBoardRepository;
+
+    @Test
+    public void insertBoard() {
+
+        IntStream.rangeClosed(1,100).forEach(i -> {
+
+            Member member = Member.builder()
+                    .member_id("u"+i)
+                    .nickname("user"+i)
+                    .pw("1111")
+                    .build();
+
+            FreeBoard freeBoard = FreeBoard.builder()
+                    .content("content....." + i )
+                    .hate_cnt((long)(Math.random()*100)) // 1부터 100사이 난수 발생
+                    .like_cnt((long)(Math.random()*100)) // 1부터 100사이 난수 발생
+                    .title("Title..." + i)
+                    .member(member)
+                    .build();
+
+            freeBoardRepository.save(freeBoard);
+
+        });
+    }
+}
