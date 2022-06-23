1cb4f923f297dac5e5300e8cc295f17b037efce7
riiine <xpzmslr98@naver.com>
2022-06-23 11:46:58 +0900

    1) 자유게시판 html 파일들 수정(UI 출력됨) 2) TestController.java에 자유게시판 관련 코드 추가

--- src/main/java/org/zerock/onmomProject/controller/TestController.java
+++ src/main/java/org/zerock/onmomProject/controller/TestController.java
@@ -25,4 +25,25 @@ public class TestController {
     public void review_read(){
     }
 
+
+    // 자유게시판 목록 페이지 테스트
+    @GetMapping("/freeBoard_list")
+    public void freeBoard_list(){
+    }
+
+    // 자유게시판 등록 페이지 테스트
+    @GetMapping("/freeBoard_register")
+    public void freeBoard_register(){
+    }
+
+    // 자유게시판 수정 페이지 테스트
+    @GetMapping("/freeBoard_modify")
+    public void freeBoard_modify(){
+    }
+
+    // 자유게시판 읽기 페이지 테스트
+    @GetMapping("/freeBoard_read")
+    public void freeBoard_read(){
+    }
+
 }
