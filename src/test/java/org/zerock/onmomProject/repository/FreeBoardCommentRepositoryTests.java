44b4f64f4c345aa64370b1a3fcfed359c8a12ad8
riiine <xpzmslr98@naver.com>
2022-06-27 11:56:14 +0900

    1) FreeBoardCommentRepositoryTests.java 충돌수정

--- src/test/java/org/zerock/onmomProject/repository/FreeBoardCommentRepositoryTests.java
+++ src/test/java/org/zerock/onmomProject/repository/FreeBoardCommentRepositoryTests.java
@@ -42,18 +42,7 @@ public class FreeBoardCommentRepositoryTests {
             freeBoardCommentRepository.save(freeBoardComment);
         });
     }
-    @Transactional
-    @Test
-    public void readReply1(){
 
-        Optional<FreeBoardComment> result = freeBoardCommentRepository.findById(1L);
-
-        FreeBoardComment freeBoardComment = result.get();
-
-
-        System.out.println(freeBoardComment);
-        System.out.println(freeBoardComment.getComment_id());
-    }
 
     @Transactional
     @Test
