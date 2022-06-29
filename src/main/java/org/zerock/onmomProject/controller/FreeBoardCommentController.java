777c859ac73ed317786640e0ff0ce3fc46420f3b
windowgod <91017486+windowgod@users.noreply.github.com>
2022-06-29 12:31:05 +0900

    22-06-29 ======================================= ※수정 사항 ====================================== +++++++++++++++++++++++++++++++++ comment 완성할것

--- src/main/java/org/zerock/onmomProject/controller/FreeBoardCommentController.java
+++ src/main/java/org/zerock/onmomProject/controller/FreeBoardCommentController.java
@@ -1,30 +1,30 @@
-//package org.zerock.onmomProject.controller;
-//
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.log4j.Log4j2;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.MediaType;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//import org.zerock.onmomProject.dto.FreeBoardCommentDTO;
-//import org.zerock.onmomProject.service.FreeBoardCommentService;
-//
-//import java.awt.*;
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/onmom/free")
-//@Log4j2
-//@RequiredArgsConstructor
-//public class FreeBoardCommentController {
-//    private final FreeBoardCommentService freeBoardCommentService;
-//
-//    @GetMapping(value = "/onmom/freeBoard{free_id}",produces = MediaType.APPLICATION_JSON_VALUE)
-//    public ResponseEntity<List<FreeBoardCommentDTO>> getListByFreeBoard(@PathVariable("free_id")Long free_id){
-//        log.info("free_id: " + free_id);
-//
-//        return new ResponseEntity<>(freeBoardCommentService.getList(free_id), HttpStatus.OK);
-//    }
+package org.zerock.onmomProject.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.zerock.onmomProject.dto.FreeBoardCommentDTO;
+import org.zerock.onmomProject.service.FreeBoardCommentService;
+
+import java.awt.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/replies/")
+@Log4j2
+@RequiredArgsConstructor
+public class FreeBoardCommentController {
+    private final FreeBoardCommentService freeBoardCommentService;
+
+    @GetMapping(value = "/onmom/freeBoard/{free_id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<FreeBoardCommentDTO>> getListByFreeBoard(@PathVariable("free_id")Long free_id){
+        log.info("free_id: " + free_id);
+
+        return new ResponseEntity<>(freeBoardCommentService.getList(free_id), HttpStatus.OK);
+    }
 //
 //    @PostMapping("")
 //    public ResponseEntity<Long> register(@RequestBody FreeBoardCommentDTO freeBoardCommentDTO){
@@ -55,5 +55,5 @@
 //
 //        return new ResponseEntity<>("success",HttpStatus.OK);
 //    }
-//}
-//
+}
+
