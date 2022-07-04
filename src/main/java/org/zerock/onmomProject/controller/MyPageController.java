c3d0bfcf15db12d2dcd7ea2c7debeefd907d0b5d
naseokju <lukas95@naver.com>
2022-07-04 14:20:54 +0900

    마이페이지 내가 쓴 글 구현 ing
    리뷰게시판 마이페이지 구현 (자유, 추천 모두 페이지 당 5개 씩)

--- src/main/java/org/zerock/onmomProject/controller/MyPageController.java
+++ src/main/java/org/zerock/onmomProject/controller/MyPageController.java
@@ -42,22 +42,19 @@ public class MyPageController {
     public String memberInfo(Model model, Principal principal,
                              FreePageRequestDTO freePageRequestDTO,
                              ReviewPageRequestDTO reviewPageRequestDTO, HttpSession session){
+
         String member_id = principal.getName();
 
-        /////////////////////////////////////////////////////내가 쓴 글 수정중
-        //MemberDTO dto = (MemberDTO)session.getAttribute("memberDTO");
-        //String member_id = dto.getMember_id();
-        //위에 두 줄 코드를 변경해야함. 지금은 그냥 이메일값 직접 대입해봄.
-//        String member_id = "seokjusproject@gmail.com";
-
+        // 회원 정보 불러오기
         MemberDTO memberInfo = memberService.selectMember(member_id);
         model.addAttribute("memberInfo", memberInfo);
-        ///////////////////////////////////////////////////////내가 쓴 글 수정중
 
-//       model.addAttribute( "result", freeBoardService.getList(freePageRequestDTO));
+        // 내가 쓴 글 자유게시판 게시물 불러오기
+        freePageRequestDTO.setSize(5);
         model.addAttribute("freeBoardDTO", freeBoardService.getMyPost(member_id, freePageRequestDTO));
 
-
+        // 내가 쓴 글 추천게시판 게시물 불러오기
+        reviewPageRequestDTO.setSize(5);
         model.addAttribute("reviewBoardDTO", reviewBoardService.getMyPost(member_id, reviewPageRequestDTO));
 
         return "onmom/member/info";
