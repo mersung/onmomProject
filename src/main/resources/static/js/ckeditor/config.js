0efca629fda13599543861bd8f95ebecd3dcb37e
riiine <xpzmslr98@naver.com>
2022-07-07 18:19:52 +0900

    1) ck에디터에서 안 쓰는 기능들 삭제 2) README 동영상 추가 삽입

--- src/main/resources/static/js/ckeditor/config.js
+++ src/main/resources/static/js/ckeditor/config.js
@@ -13,16 +13,16 @@ CKEDITOR.editorConfig = function( config ) {
 		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
 		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
 		{ name: 'links' },
-		{ name: 'insert' },
+//		{ name: 'insert' },
 		{ name: 'forms' },
 		{ name: 'tools' },
 		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
 		{ name: 'others' },
 		'/',
 		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-		{ name: 'styles' },
-		{ name: 'colors' },
+//		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+//		{ name: 'styles' },
+//		{ name: 'colors' },
 		{ name: 'about' }
 	];
 
